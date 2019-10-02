4bda0f92cadd8267cca1350e363f06dddbe628d8
DuckyBoy343 <50601984+DuckyBoy343@users.noreply.github.com>
2019-10-02 18:57:55 -0500

    xd

--- I wanna die/I wanna die/Form1.cs
+++ I wanna die/I wanna die/Form1.cs
@@ -129,7 +129,7 @@ namespace I_wanna_die
                 Cur = Peano(n - 1, Cur, L / 4.0);
                 Cur.angulo += Math.PI / 4.0;
                 Cur.angulo += Math.PI / 4.0;
-                Cur.angulo = Math.PI / 4.0;
+                Cur.angulo -= Math.PI / 4.0;
                 //Cur = Peano(n - 1, Cur, L / 4.0);
             }
             return Cur;
