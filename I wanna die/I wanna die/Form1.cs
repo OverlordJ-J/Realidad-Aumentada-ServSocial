9b37cae43b2e7472b6944b2e3dcaf336fde06346
DuckyBoy343 <50601984+DuckyBoy343@users.noreply.github.com>
2019-10-02 20:40:16 -0500

    Nice

--- I wanna die/I wanna die/Form1.cs
+++ I wanna die/I wanna die/Form1.cs
@@ -55,10 +55,12 @@ namespace I_wanna_die
         private void glControl1_Paint(object sender, PaintEventArgs e)
         {
             double h = 300.0 + (600.0 * Math.Sin(-2.0 * Math.PI / 3.0));
-            Cursor C1 = new Cursor(-0.0, 100.0, 5.0*Math.PI/2);
+            Cursor C1 = new Cursor(-200.0, 0.0, 1.0*Math.PI/2.0);
             //Cursor C2 = new Cursor( 200.0, 200.0, -2.0 * Math.PI/3.0);
-            Cursor C2 = new Cursor(200.0, 200.0, -2.0*Math.PI / 2.0);
-            Cursor C3 = new Cursor(0.0, h, -Math.PI/2.0);
+            Cursor C2 = new Cursor(-0.0, 300.0, -2.0*Math.PI / 2.0);
+            Cursor C3 = new Cursor(0.0, -0.0, Math.PI/2.0);
+            Cursor C4 = new Cursor(200.0, -0.0, 2.0*Math.PI / 2.0);
+            Cursor C5 = new Cursor(200.0, -0.0, Math.PI / 2.0);
             //C1.P.x = -450.0; C1.P.y = 0.0; C1.angulo = 0.0;
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
@@ -67,9 +69,11 @@ namespace I_wanna_die
             //Koch(iteracion, C2, 600.0);
             //Koch(iteracion, C3, 600.0);
 
-            Peano(iteracion, C1, 600.0);
-            Peano(iteracion, C2, 600.0);
-            Peano(iteracion, C3, 600.0);
+            Peano(iteracion, C1, 300.0);
+            Peano(iteracion, C2, 200.0);
+            Peano(iteracion, C3, 300.0);
+            Peano(iteracion, C4, 200.0);
+            Peano(iteracion, C5, 300.0);
 
             glControl1.SwapBuffers();
         }
