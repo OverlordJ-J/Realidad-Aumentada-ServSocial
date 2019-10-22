346fea5710b7963898152deced8bcbd40e084671
DuckyBoy343 <50601984+DuckyBoy343@users.noreply.github.com>
2019-10-22 15:59:10 -0500

    Arbol y chuky

--- Graficacion/Practica3/Practica3/Form1.cs
+++ Graficacion/Practica3/Practica3/Form1.cs
@@ -13,8 +13,8 @@ namespace Practica3
 {
     public partial class Form1 : Form
     {
-        double xmax = 500.0, ymax = 500.0, zmax = 1.0;
-        double iteracion = 0;
+        double xmax = 100.0, ymax = 100.0, zmax = 1.0;
+        int iteracion = 0;
         public struct Punto
         {
             public double x, y, z;
@@ -46,78 +46,42 @@ namespace Practica3
 
         private void glControl1_Paint(object sender, PaintEventArgs e)
         {
-            Punto A = new Punto(-50.0, -50.0, 0.0);
-            Punto B = new Punto(50.0, -50.0, 0.0);
-            Punto C = new Punto(0.0, 50.0, 0.0);
+            //Punto A = new Punto(-50.0, -50.0, 0.0);
+            //Punto B = new Punto(50.0, -50.0, 0.0);
+            //Punto C = new Punto(0.0, 50.0, 0.0);
 
-            Punto AP = new Punto(100.0, 100.0, 0.0);
-            Punto BP = new Punto(-100.0, 100.0, 0.0);
-            Punto CP = new Punto(-100.0, -100.0, 0.0);
-            Punto DP = new Punto(100.0, -100.0, 0.0);
+            Punto AP = new Punto(25.0, 25.0, 0.0);
+            Punto BP = new Punto(-25.0, 25.0, 0.0);
+            Punto CP = new Punto(-25.0, -25.0, 0.0);
+            Punto DP = new Punto(25.0, -25.0, 0.0);
+
+            Punto AS = new Punto(300.0, 300.0, 0.0);
+            Punto BS = new Punto(-300.0, 300.0, 0.0);
+            Punto CS = new Punto(-300.0, -300.0, 0.0);
+            Punto DS = new Punto(300.0, -300.0, 0.0);
+
+            Punto AC = new Punto(-50.0, -50.0, 0.0);
+
+            Punto A = new Punto(-0.0, -0.0, 0.0);
+            Punto B = new Punto(0, 0, 0);
+
+            double side = 15;
+            A.x = 6 * side / 2 - side / 2;
+            A.y = 4 * side;
+            B.x = 6 * side / 2 + side / 2;
+            B.y = 4 * side;
+
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
             //sierpinsky(iteracion, A, B, C);
-            // Pitagoras(iteracion, AP, BP, CP, DP);
-            sierpinskyCarpet(iteracion, AP, BP, CP, DP);
+            //Pitagoras(iteracion, AP, BP, CP, DP);
+            //Carpet(iteracion, AS, BS, CS, DS);
+            PitTree(iteracion, A, B);
+            //SierCarpet(iteracion, AC, 100.0);
+            //Cuadrado(AP, (double)iteracion, Math.PI / 4);
+
             glControl1.SwapBuffers();
-        }
-
-        void sierpinskyCarpet(double n, Punto A, Punto B, Punto C,Punto D)
-        {
-            Punto AB = new Punto(), BC = new Punto(), CD = new Punto(), DA=new Punto(), AC2 = new Punto(), AC = new Punto(), BD = new Punto(), BD2 = new Punto();
-            if (n == 0)
-            {
-                GL.Begin(PrimitiveType.Polygon);
-                GL.Color3(Color.Green);
-                GL.Vertex2(A.x, A.y);
-                GL.Vertex2(B.x, B.y);
-                GL.Vertex2(C.x, C.y);
-                GL.Vertex2(D.x, D.y);
-                GL.End();
-            }
-            else
-            {
-                
-                AC = PuntoMedio2(A, B);
-                BD = PuntoMedio2(B, D);
-                AC2 = PuntoMedio3(A, C);
-                BD2 = PuntoMedio3(B, D);
-
-
-                sierpinskyCarpet(n - 1, A, B, AC, BD);
-                sierpinskyCarpet(n - 1, AC, B, AC2, BD);
-                sierpinskyCarpet(n - 1, AC2, B, C, BD);
-                sierpinskyCarpet(n - 1, A, BD, AC, BD2);
-                sierpinskyCarpet(n - 1, AC2, BD, C, BD2);
-                sierpinskyCarpet(n - 1, A, BD2, AC, D);
-                sierpinskyCarpet(n - 1,AC,BD2,AC2,D);
-                sierpinskyCarpet(n - 1,AC2,BD2,C,D);
-
-
-                //////////////Code////////////////
-                //alfombrizar(g, x1, y1, (x2 - x1) / 3 + x1, (y2 - y1) / 3 + y1, fases????? );
-                //alfombrizar(g, (x2 - x1) / 3 + x1, y1, (x2 - x1) * 2 / 3 + x1, (y2 - y1) / 3 + y1, f);
-                //alfombrizar(g, (x2 - x1) * 2 / 3 + x1, y1, x2, (y2 - y1) / 3 + y1, f);
-                //alfombrizar(g, x1, (y2 - y1) / 3 + y1, (x2 - x1) / 3 + x1, (y2 - y1) * 2 / 3 + y1, f);
-                //alfombrizar(g, (x2 - x1) * 2 / 3 + x1, (y2 - y1) / 3 + y1, x2, (y2 - y1) * 2 / 3 + y1, f);
-                //alfombrizar(g, x1, (y2 - y1) * 2 / 3 + y1, (x2 - x1) / 3 + x1, y2, f);
-                //alfombrizar(g, (x2 - x1) / 3 + x1, (y2 - y1) * 2 / 3 + y1, (x2 - x1) * 2 / 3 + x1, y2, f);
-                //alfombrizar(g, (x2 - x1) * 2 / 3 + x1, (y2 - y1) * 2 / 3 + y1, x2, y2, f);
-
-            }
-
-        }
-
-        Punto PuntoMedio3(Punto P0, Punto P1)
-        {
-            Punto P = new Punto(0, 0, 0);
-
-            P.x = ((P1.x - P0.x) *2 / 3.0 + P0.x);
-            P.y = ((P1.y - P0.y) *2 / 3.0 + P0.y);
-            //P.z = P0.z + ((P1.z - P0.z) / 2.0);
-
-            return P;
         }
 
         void sierpinsky(double n, Punto A, Punto B, Punto C)
@@ -156,7 +120,7 @@ namespace Practica3
             GL.Vertex2(C.x, C.y);
             GL.Vertex2(D.x, D.y);
             GL.End();
-
+            //}
             if (n != 0)
             {
                 PE = E(B, A);
@@ -168,6 +132,112 @@ namespace Practica3
                 Pitagoras(n - 1, PG, PF, B, PE);
                 Pitagoras(n - 1, PH, PI, PE, A);
             }
+        }
+
+        void Carpet(double n, Punto A, Punto B, Punto C, Punto D)
+        {
+
+            Punto AB = new Punto(), BC = new Punto(), CD = new Punto(), DA = new Punto();
+
+            if (n == 0)
+            {
+                GL.Begin(PrimitiveType.Quads);
+                GL.Color3(Color.Black);
+                GL.Vertex2(A.x, A.y);
+                GL.Vertex2(B.x, B.y);
+                GL.Vertex2(C.x, C.y);
+                GL.Vertex2(D.x, D.y);
+                GL.End();
+            }
+            else
+            {
+                AB = CuadradoMid(A, B);
+                BC = CuadradoMid(B, C);
+                CD = CuadradoMid(C, D);
+                DA = CuadradoMid(D, A);
+
+                Carpet(n - 1, AB, BC, CD, DA);
+            }
+        }
+
+        void PitTree(int n, Punto A, Punto B)
+        {
+            Punto a = A;
+            Punto b = B;
+            Punto c = new Punto();
+            Punto d = new Punto();
+            Punto e = new Punto();
+
+            c.x = b.x - (a.y - b.y);
+            c.y = b.y - (b.x - a.x);
+
+            d.x = a.x - (a.y - b.y);
+            d.y = a.y - (b.x - a.x);
+
+            e.x = d.x + (b.x - a.x - (a.y - b.y)) / 2;
+            e.y = d.y - (b.x - a.x + a.y - b.y) / 2;
+
+            if (n > 0)
+            {
+                GL.Begin(PrimitiveType.Quads);
+                GL.Color3(Color.Navy);
+                GL.Vertex2(a.x * -1, a.y * -1);
+                GL.Vertex2(b.x * -1, b.y * -1);
+                GL.Vertex2(c.x * -1, c.y * -1);
+                GL.Vertex2(d.x * -1, d.y * -1);
+                GL.End();
+
+
+                PitTree(n - 1, d, e);
+                PitTree(n - 1, e, c);
+            }
+        }
+
+        void SierCarpet(int n, Punto A, double tam)
+        {
+            if (n == 0)
+            {
+                GL.Begin(PrimitiveType.Quads);
+                GL.Color3(Color.Green);
+                GL.Vertex2(A.x, A.y);
+                GL.Vertex2(A.x, A.y + tam);
+                GL.Vertex2(A.x + tam, A.y + tam);
+                GL.Vertex2(A.x + tam, A.y);
+                GL.End();
+
+            }
+            else
+            {
+                Punto temP = A;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (j == 1 && i == 1)
+                        {
+
+                        }
+                        else
+                            SierCarpet(n - 1, temP, tam / 3.0);
+                            temP.y += tam / 3.0;
+                    }
+                    temP.y = A.y;
+                    temP.x += tam / 3.0;
+                }
+            }
+        }
+
+        Punto CuadradoMid(Punto p1, Punto p2)
+        {
+            Punto Cuad = new Punto(0, 0, 0);
+            Punto mid = new Punto(0, 0, 0);
+
+            mid = PuntoMedio(p1, p2);
+
+            Cuad.x = (mid.x - ((p1.x - p2.x) / 6)) + ((p1.x - p2.x) / 2);
+            Cuad.y = (mid.y - ((p1.y - p2.y) / 6)) + ((p1.y - p2.y) / 2);
+
+            return Cuad;
         }
 
         //Punto Hipo(Punto p1, Punto p2)
@@ -198,10 +268,10 @@ namespace Practica3
         {
             Punto PE = new Punto(0, 0, 0);
 
-            PE.x = p1.x + Math.Cos(Math.PI / 3) * Math.Cos(Math.PI / 3) * Math.Sqrt(p1.x * p1.x) + Math.Sqrt(p2.x * p2.x);
+            PE.x = p1.x + Math.Cos(Math.PI / 4) * Math.Cos(Math.PI / 4) * (Math.Sqrt(p1.x * p1.x) + Math.Sqrt(p2.x * p2.x));
             //PE.x = p1.x + Math.Cos(alpha) * Math.Cos(alpha) * Math.Sqrt(p1.x * p1.x) - Math.Sqrt(p2.x * p2.x);
 
-            PE.y = p1.y + Math.Sin(Math.PI / 3) * Math.Sin(Math.PI / 3) * Math.Sqrt(p1.y * p1.y) + Math.Sqrt(p2.y * p2.y);
+            PE.y = p1.y + Math.Sin(Math.PI / 4) * Math.Sin(Math.PI / 4) * (Math.Sqrt(p1.y * p1.y) + Math.Sqrt(p2.y * p2.y));
             //PE.y = p1.y + Math.Sin(alpha) * Math.Sin(alpha) * Math.Sqrt(p1.y * p1.y) - Math.Sqrt(p2.y * p2.y);
 
             return PE;
@@ -211,10 +281,10 @@ namespace Practica3
         {
             Punto PF = new Punto(0, 0, 0);
 
-            PF.x = p1.x + (Math.Cos((Math.PI / 3) + (Math.PI / 2))) * Math.Sqrt(PE.x * PE.x) + Math.Sqrt(p1.x * p1.x);
+            PF.x = p1.x + (Math.Cos((Math.PI / 4) + (Math.PI / 2))) * (Math.Sqrt(PE.x * PE.x) + Math.Sqrt(p1.x * p1.x));
             //PF.x = p1.x + Math.Cos((alpha) + (Math.PI / 2)) * Math.Sqrt(PE.x * PE.x) + Math.Sqrt(p1.x * p1.x);
 
-            PF.y = p1.y + (Math.Sin((Math.PI / 3) + (Math.PI / 2))) * Math.Sqrt(PE.y * PE.y) + Math.Sqrt(p1.y * p1.y);
+            PF.y = p1.y + (Math.Sin((Math.PI / 4) + (Math.PI / 2))) * (Math.Sqrt(PE.y * PE.y) + Math.Sqrt(p1.y * p1.y));
             //PF.y = p1.y + Math.Sin((alpha) + (Math.PI / 2)) * Math.Sqrt(PE.y * PE.y) + Math.Sqrt(p1.y * p1.y);
 
             return PF;
@@ -224,8 +294,8 @@ namespace Practica3
         {
             Punto PG = new Punto(0, 0, 0);
 
-            PG.x = PE.x + (Math.Cos((Math.PI / 3) + (Math.PI / 2))) * Math.Sqrt(PE.x * PE.x) + Math.Sqrt(p1.x * p1.x);
-            PG.y = PE.y + (Math.Sin((Math.PI / 3) + (Math.PI / 2))) * Math.Sqrt(PE.y * PE.y) + Math.Sqrt(p1.y * p1.y);
+            PG.x = PE.x + (Math.Cos((Math.PI / 4) + (Math.PI / 2))) * (Math.Sqrt(PE.x * PE.x) + Math.Sqrt(p1.x * p1.x));
+            PG.y = PE.y + (Math.Sin((Math.PI / 4) + (Math.PI / 2))) * (Math.Sqrt(PE.y * PE.y) + Math.Sqrt(p1.y * p1.y));
 
             return PG;
         }
@@ -234,8 +304,8 @@ namespace Practica3
         {
             Punto PH = new Punto(0, 0, 0);
 
-            PH.x = p2.x + Math.Cos(Math.PI / 3) * Math.Sqrt(PE.x * PE.x) + Math.Sqrt(p2.x * p2.x);
-            PH.y = p2.y + Math.Sin(Math.PI / 3) * Math.Sqrt(PE.y * PE.y) + Math.Sqrt(p2.y * p2.y);
+            PH.x = p2.x + Math.Cos(Math.PI / 4) * (Math.Sqrt(PE.x * PE.x) + Math.Sqrt(p2.x * p2.x));
+            PH.y = p2.y + Math.Sin(Math.PI / 4) * (Math.Sqrt(PE.y * PE.y) + Math.Sqrt(p2.y * p2.y));
 
             return PH;
         }
@@ -244,21 +314,10 @@ namespace Practica3
         {
             Punto PI = new Punto(0, 0, 0);
 
-            PI.x = PE.x + Math.Cos(Math.PI / 3) * Math.Sqrt(PE.x * PE.x) + Math.Sqrt(p2.x * p2.x);
-            PI.y = PE.y + Math.Sin(Math.PI / 3) * Math.Sqrt(PE.y * PE.y) + Math.Sqrt(p2.y * p2.y);
+            PI.x = PE.x + Math.Cos(Math.PI / 4) * (Math.Sqrt(PE.x * PE.x) + Math.Sqrt(p2.x * p2.x));
+            PI.y = PE.y + Math.Sin(Math.PI / 4) * (Math.Sqrt(PE.y * PE.y) + Math.Sqrt(p2.y * p2.y));
 
             return PI;
-        }
-
-        Punto PuntoMedio2(Punto P0, Punto P1)
-        {
-            Punto P = new Punto(0, 0, 0);
-
-            P.x = ((P1.x - P0.x) / 3.0 + P0.x);
-            P.y = ((P1.y - P0.y) / 3.0 + P0.y); 
-            //P.z = P0.z + ((P1.z - P0.z) / 2.0);
-
-            return P;
         }
 
         Punto PuntoMedio(Punto P0, Punto P1)
@@ -290,6 +349,28 @@ namespace Practica3
                     break;
             }
             glControl1.Invalidate();
+        }
+
+        void Cuadrado(Punto C, double r, double ang)
+        {
+            Punto[] V = new Punto[4];
+            double theta = ang;
+            int i;
+
+            for (i = 0; i < 4; i++)
+            {
+                V[i].x = C.x + (r * Math.Cos(theta));
+                V[i].y = C.y + (r * Math.Sin(theta));
+                V[i].z = 0.0;
+                theta += Math.PI / 2;
+            }
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.Vertex3(V[0].x, V[0].y, V[0].z);
+            GL.Vertex3(V[1].x, V[1].y, V[1].z);
+            GL.Vertex3(V[2].x, V[2].y, V[2].z);
+            GL.Vertex3(V[3].x, V[3].y, V[3].z);
+            GL.End();
         }
     }
 }
